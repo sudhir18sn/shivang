7081485fd775883b20b658d30732bbcb64eb527f
Shivang Mishra <githubshiv@gmail.com>
2025-01-18 13:15:29 +0530

    Update start.py

--- PROFESSOR/plugins/bot/start.py
+++ PROFESSOR/plugins/bot/start.py
@@ -34,7 +34,6 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgQAAxkBAAEQ245ljYcpjiUzNlnqZayXwYGXQdQUYgAC2Q8AAnsbSFJTlxo-p_AUAAEzBA")
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
