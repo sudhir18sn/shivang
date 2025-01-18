3178f0a29182de2bf69e8bb440da0e47bbe2cf6a
Shivang Mishra <githubshiv@gmail.com>
2025-01-18 15:25:37 +0530

    Update sudoers.py

--- PROFESSOR/plugins/sudo/sudoers.py
+++ PROFESSOR/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝐊𝐘𝐀 𝐑𝐄 𝐑𝐀𝐍𝐃𝐈 𝐊𝐄 𝐒𝐔𝐃𝐎 𝐋𝐈𝐒𝐓 𝐃𝐄𝐊𝐇𝐄𝐆𝐀 𝐓𝐄𝐑𝐈 𝐌𝐀 𝐊𝐈 𝐂𝐇𝐔𝐓 @II_Hunter_II", show_alert=True)
+        return await callback_query.answer("kyu dekhna h ", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
