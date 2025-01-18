3211a86ea1c239bf306b6e6a882bf3904b129009
Shivang Mishra <githubshiv@gmail.com>
2025-01-18 14:38:46 +0530

    Update Welcome.py

--- PROFESSOR/plugins/tools/Welcome.py
+++ PROFESSOR/plugins/tools/Welcome.py
@@ -137,7 +137,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ┣ 𝟓 ➥ 𝐆ɪᴠᴇ  ʀᴇsᴘᴇᴄᴛ , ᴛᴀᴋᴇ  ʀᴇsᴘᴇᴄᴛ 
 ┗━━━━━━━━━━━━━━━━━      
 
-❖ ᴘᴏᴡᴇʀᴇᴅ  ➥ <a href=t.me/riya_network>тҽαɱ ɾιყα</a>
+
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"✧ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✧", url=f"https://t.me/{app.username}?startgroup=true")]])
         )
