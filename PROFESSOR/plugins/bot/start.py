8671bcfe31e30c5cd589bec47908443951ff0d4a
Shivang Mishra <githubshiv@gmail.com>
2025-01-18 13:16:31 +0530

    Update start.py

--- PROFESSOR/plugins/bot/start.py
+++ PROFESSOR/plugins/bot/start.py
@@ -86,7 +86,6 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAxkBAAEQI1hlTLog9AN9m9USFpWRCMlU8iMCVwACbQQAAjYSmFa-LfaOxMHalzME")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
